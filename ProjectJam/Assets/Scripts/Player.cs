52dec59aeeadba60afac85b46e5cb2f239a6931b
DevDorf <caiorodrigues1989@gmail.com>
2023-07-01 02:28:18 -0300

    Update Movimentação
    Foram adicionados ao script do player as funções Move e Jump.

--- ProjectJam/Assets/Scripts/Player.cs
+++ ProjectJam/Assets/Scripts/Player.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
     public float Speed;
+    public float JumpForce;
+
+    public bool isJumping;
+    public bool doubleJump;
+
+    private Rigidbody2D rig;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move ();
+        Move();
+        Jump();
     }
 
     void Move()
@@ -24,4 +32,40 @@ public class Player : MonoBehaviour
         transform.position += movement * Time.deltaTime * Speed;
     }
 
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (!isJumping)
+            {
+                rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+                doubleJump = true;// mudando para false, pula apenas uma vez.
+            }
+            else
+            {
+                if (doubleJump)
+                {
+                     rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+                     doubleJump = false;
+                }
+            }
+            
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 8)
+        {
+            isJumping = false;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 8)
+        {
+            isJumping = true;
+        }
+    }
 }
